11cd59e990d493d62f255f05e999068f9d60f448
Alex Carter <victoryforphil@gmail.com>
2026-01-16 00:57:26 -0800

    Image views

--- game-of-questioning-life/game/include/vfp_vulkan_device.h
+++ game-of-questioning-life/game/include/vfp_vulkan_device.h
@@ -16,6 +16,14 @@ typedef struct VfpDeviceVulkan {
     VkQueue graphicsQueue;
     VkQueue presentQueue;
     VkSurfaceKHR surface;
+    VkSwapchainKHR swapchain;
+    VkImage *swapchainImages;
+    uint32_t swapchainImageCount;
+    VkFormat swapchainImageFormat;
+    VkExtent2D swapchainExtent;
+
+    VkImageView *swapchainImageViews;
+    uint32_t swapchainImageViewCount;
 
 } VfpDeviceVulkan;
 
@@ -26,6 +34,12 @@ typedef struct VfpVkQueueFamilyIndices {
     uint32_t presentFamily;
     bool bPresentFamilySet;
 } VfpVkQueueFamilyIndices;
+
+typedef struct VfpVkSwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    VkSurfaceFormatKHR *formats;
+    VkPresentModeKHR *presentModes;
+} VfpVkSwapChainSupportDetails;
 
 void vfp_vk_create_queue_family_index(VfpVkQueueFamilyIndices *out_indices,
                                       int32_t familyIndex);
@@ -39,6 +53,17 @@ static const char *const VFP_VK_VALIDATE_DEBUG_LAYERS[] = {
 static const uint32_t N_VFP_VK_VALIDATE_DEBUG_LAYERS =
     sizeof(VFP_VK_VALIDATE_DEBUG_LAYERS) /
     sizeof(VFP_VK_VALIDATE_DEBUG_LAYERS[0]);
+
+// TODO: Macro for statisc arrays
+static const char *const VFP_VK_DEVICE_EXTENSIONS[] = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+};
+
+static const uint32_t N_VFP_VK_DEVICE_EXTENSIONS =
+    sizeof(VFP_VK_DEVICE_EXTENSIONS) / sizeof(VFP_VK_DEVICE_EXTENSIONS[0]);
+
+const char *vfp_vk_result_string(VkResult result);
+void vfp_vk_log_result(const char *label, VkResult result);
 
 VfpError vfp_vulkan_device_create(VfpDeviceVulkan *out_device,
                                   GLFWwindow *window);
@@ -57,7 +82,29 @@ VfpError vfp_vk_find_queue_families(VfpVkQueueFamilyIndices *out_indices,
 
 VfpError vfp_vk_create_logical_device(VfpDeviceVulkan *pDevice);
 
-const char *vfp_vk_result_string(VkResult result);
-void vfp_vk_log_result(const char *label, VkResult result);
+VfpError vfp_vfk_create_device_ext_support(VkPhysicalDevice physicalDevice,
+                                           bool *out_isSupported);
+
+VfpError
+vfp_vk_query_swapchain_support(VfpVkSwapChainSupportDetails *out_details,
+                               VkPhysicalDevice physicalDevice,
+                               VkSurfaceKHR surface);
+
+VfpError
+vfp_vk_choose_swap_surface_format(const VkSurfaceFormatKHR *availableFormats,
+                                  uint32_t formatCount,
+                                  VkSurfaceFormatKHR *out_format);
+
+VfpError
+vfp_vk_choose_swap_present_mode(const VkPresentModeKHR *availablePresentModes,
+                                uint32_t presentModeCount,
+                                VkPresentModeKHR *out_presentMode);
+
+VfpError vfp_vk_choose_swap_extent(const VkSurfaceCapabilitiesKHR *capabilities,
+                                   VkExtent2D *out_extent);
+
+VfpError vfp_vk_create_swapchain(VfpDeviceVulkan *pDevice);
+
+VfpError vfp_vfk_create_image_views(VfpDeviceVulkan *pDevice);
 
 #endif // _VFP_VULKAN_DEVICE_H_
--- game-of-questioning-life/game/src/vfp_vulkan_device.c
+++ game-of-questioning-life/game/src/vfp_vulkan_device.c
@@ -165,6 +165,17 @@ VfpError vfp_vulkan_device_create(VfpDeviceVulkan *out_device,
         return VFP_ERROR_GENERIC;
     }
 
+    printf("VulkanDevice // Creating swapchain...\n");
+    if (vfp_vk_create_swapchain(out_device) != VFP_OK) {
+        fprintf(stderr, "Game // Vulkan // Failed to create swapchain\n");
+        return VFP_ERROR_GENERIC;
+    }
+
+    printf("VulkanDevice // Creating image views...\n");
+    if (vfp_vfk_create_image_views(out_device) != VFP_OK) {
+        fprintf(stderr, "Game // Vulkan // Failed to create image views\n");
+        return VFP_ERROR_GENERIC;
+    }
     return VFP_OK;
 }
 
@@ -175,14 +186,17 @@ VfpError vfp_vulkan_device_destroy(VfpDeviceVulkan *device) {
                         "NULL Vulkan device\n");
         return VFP_ERROR_GENERIC;
     }
-
+    vkDestroySwapchainKHR(device->logicalDevice, device->swapchain, NULL);
     // Null Check *device->instance - if null, return w/ warning
     if (device->pInstance == NULL) {
         fprintf(stderr, "Game // Vulkan // Warning: Attempted to destroy a "
                         "Vulkan device with NULL instance\n");
         return VFP_ERROR_GENERIC;
     }
-
+    for (uint32_t i = 0; i < device->swapchainImageViewCount; i++) {
+        vkDestroyImageView(device->logicalDevice,
+                           device->swapchainImageViews[i], NULL);
+    }
     // Null check and free logical device
     if (device->logicalDevice != VK_NULL_HANDLE) {
         vkDestroyDevice(device->logicalDevice, NULL);
@@ -192,6 +206,10 @@ VfpError vfp_vulkan_device_destroy(VfpDeviceVulkan *device) {
     if (device->surface != VK_NULL_HANDLE) {
         vkDestroySurfaceKHR(device->pInstance, device->surface, NULL);
         // device->surface = VK_NULL_HANDLE;
+    }
+    for (uint32_t i = 0; i < device->swapchainImageViewCount; i++) {
+        vkDestroyImageView(device->logicalDevice,
+                           device->swapchainImageViews[i], NULL);
     }
     vkDestroyInstance(device->pInstance, NULL);
     device->pInstance = NULL;
@@ -314,6 +332,40 @@ VfpError vfp_vk_physical_suitable(VfpDeviceVulkan *pDevice,
         return VFP_OK;
     }
 
+    if (!queueFamilyIndices.bPresentFamilySet) {
+        printf("Vulkan // Physical Device %s does not have required queue "
+               "families.\n",
+               deviceProperties.deviceName);
+        *out_isSuitable = false;
+        return VFP_OK;
+    }
+
+    bool extensionsSupported = false;
+    vfp_vfk_create_device_ext_support(physicalDevice, &extensionsSupported);
+    if (!extensionsSupported) {
+        printf("Vulkan // Physical Device %s does not support required device "
+               "extensions.\n",
+               deviceProperties.deviceName);
+        *out_isSuitable = false;
+        return VFP_OK;
+    }
+
+    bool swapChainAdequate = false;
+    VfpVkSwapChainSupportDetails swapChainSupport = {};
+    vfp_vk_query_swapchain_support(&swapChainSupport, physicalDevice,
+                                   pDevice->surface);
+    if (swapChainSupport.formats != NULL &&
+        swapChainSupport.presentModes != NULL) {
+        swapChainAdequate = true;
+    }
+
+    if (!swapChainAdequate) {
+        printf("Vulkan // Physical Device %s does not have adequate swapchain "
+               "support.\n",
+               deviceProperties.deviceName);
+        *out_isSuitable = false;
+        return VFP_OK;
+    }
     // First check for any dedicated GPUs and use that
     if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
         deviceFeatures.geometryShader) {
@@ -418,6 +470,7 @@ VfpError vfp_vk_create_logical_device(VfpDeviceVulkan *pDevice) {
     queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
     queueCreateInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily;
     queueCreateInfo.queueCount = 1;
+
     queueCreateInfo.pQueuePriorities = &queuePriority;
 
     VkDeviceQueueCreateInfo createInfos[2] = {queueCreateInfo};
@@ -443,7 +496,8 @@ VfpError vfp_vk_create_logical_device(VfpDeviceVulkan *pDevice) {
     createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
     createInfo.pQueueCreateInfos = createInfos;
     createInfo.queueCreateInfoCount = nCreateInfos;
-
+    createInfo.enabledExtensionCount = N_VFP_VK_DEVICE_EXTENSIONS;
+    createInfo.ppEnabledExtensionNames = VFP_VK_DEVICE_EXTENSIONS;
     createInfo.pEnabledFeatures = &deviceFeatures;
 
     VkResult result = vkCreateDevice(pDevice->physicalDevice, &createInfo, NULL,
@@ -464,5 +518,261 @@ VfpError vfp_vk_create_logical_device(VfpDeviceVulkan *pDevice) {
     vkGetDeviceQueue(pDevice->logicalDevice, queueFamilyIndices.presentFamily,
                      0, &pDevice->presentQueue);
 
+    return VFP_OK;
+}
+
+VfpError vfp_vfk_create_device_ext_support(VkPhysicalDevice physicalDevice,
+                                           bool *out_isSupported) {
+
+    uint32_t extensionCount = 0;
+    vkEnumerateDeviceExtensionProperties(physicalDevice, NULL, &extensionCount,
+                                         NULL);
+
+    VkExtensionProperties *availableExtensions =
+        malloc(sizeof(VkExtensionProperties) * extensionCount);
+    vkEnumerateDeviceExtensionProperties(physicalDevice, NULL, &extensionCount,
+                                         availableExtensions);
+    // Check for required extensions
+    for (size_t i = 0; i < N_VFP_VK_DEVICE_EXTENSIONS; i++) {
+        bool extensionFound = false;
+        for (uint32_t j = 0; j < extensionCount; j++) {
+            if (strcmp(VFP_VK_DEVICE_EXTENSIONS[i],
+                       availableExtensions[j].extensionName) == 0) {
+                extensionFound = true;
+                break;
+            }
+        }
+        if (!extensionFound) {
+            fprintf(stderr,
+                    "VulkanDevice // Required device extension not found: %s\n",
+                    VFP_VK_DEVICE_EXTENSIONS[i]);
+            free(availableExtensions);
+            *out_isSupported = false;
+            return VFP_OK;
+        }
+    }
+
+    *out_isSupported = true;
+    return VFP_OK;
+}
+
+VfpError
+vfp_vk_query_swapchain_support(VfpVkSwapChainSupportDetails *out_details,
+                               VkPhysicalDevice physicalDevice,
+                               VkSurfaceKHR surface) {
+    printf("VulkanDevice // Querying swapchain support...\n");
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface,
+                                              &out_details->capabilities);
+
+    uint32_t formatCount;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount,
+                                         NULL);
+    printf("VulkanDevice // Found %d surface formats.\n", formatCount);
+    if (formatCount != 0) {
+        out_details->formats = malloc(sizeof(VkSurfaceFormatKHR) * formatCount);
+        vkGetPhysicalDeviceSurfaceFormatsKHR(
+            physicalDevice, surface, &formatCount, out_details->formats);
+    }
+
+    uint32_t presentModeCount;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface,
+                                              &presentModeCount, NULL);
+    printf("VulkanDevice // Found %d present modes.\n", presentModeCount);
+
+    if (presentModeCount != 0) {
+        out_details->presentModes =
+            malloc(sizeof(VkPresentModeKHR) * presentModeCount);
+        vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface,
+                                                  &presentModeCount,
+                                                  out_details->presentModes);
+    }
+
+    return VFP_OK;
+}
+
+VfpError
+vfp_vk_choose_swap_surface_format(const VkSurfaceFormatKHR *availableFormats,
+                                  uint32_t formatCount,
+                                  VkSurfaceFormatKHR *out_format) {
+    // Perfer SRGB format
+    for (uint32_t i = 0; i < formatCount; i++) {
+        if (availableFormats[i].format == VK_FORMAT_B8G8R8A8_SRGB &&
+            availableFormats[i].colorSpace ==
+                VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+            *out_format = availableFormats[i];
+            return VFP_OK;
+        }
+    }
+    // else return first format
+    *out_format = availableFormats[0];
+    ;
+    return VFP_OK;
+}
+
+VfpError
+vfp_vk_choose_swap_present_mode(const VkPresentModeKHR *availablePresentModes,
+                                uint32_t presentModeCount,
+                                VkPresentModeKHR *out_presentMode) {
+    printf("VulkanDevice // Choosing swap present mode...\n");
+    // If mailbox present mode is available, use it
+    for (uint32_t i = 0; i < presentModeCount; i++) {
+        if (availablePresentModes[i] == VK_PRESENT_MODE_MAILBOX_KHR) {
+            *out_presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
+            printf("VulkanDevice // Chose MAILBOX present mode.\n");
+            return VFP_OK;
+        }
+    }
+    // Else use FIFO present mode
+    *out_presentMode = VK_PRESENT_MODE_FIFO_KHR;
+    printf("VulkanDevice // Chose FIFO present mode.\n");
+    return VFP_OK; // V-Sync
+}
+
+VfpError vfp_vk_choose_swap_extent(const VkSurfaceCapabilitiesKHR *capabilities,
+                                   VkExtent2D *out_extent) {
+    printf("VulkanDevice // Choosing swap extent...\n");
+    if (capabilities->currentExtent.width != UINT32_MAX) {
+        *out_extent = capabilities->currentExtent;
+    } else {
+        int glfwG, glfwH;
+        glfwGetFramebufferSize(glfwGetCurrentContext(), &glfwG, &glfwH);
+        VkExtent2D actualExtent = {glfwG, glfwH};
+
+        if (actualExtent.width < capabilities->minImageExtent.width) {
+            actualExtent.width = capabilities->minImageExtent.width;
+        } else if (actualExtent.width > capabilities->maxImageExtent.width) {
+            actualExtent.width = capabilities->maxImageExtent.width;
+        }
+
+        if (actualExtent.height < capabilities->minImageExtent.height) {
+            actualExtent.height = capabilities->minImageExtent.height;
+        } else if (actualExtent.height > capabilities->maxImageExtent.height) {
+            actualExtent.height = capabilities->maxImageExtent.height;
+        }
+
+        *out_extent = actualExtent;
+    }
+
+    printf("VulkanDevice // Chose extent: %d x %d\n", out_extent->width,
+           out_extent->height);
+    return VFP_OK;
+}
+
+VfpError vfp_vk_create_swapchain(VfpDeviceVulkan *pDevice) {
+    printf("VulkanDevice // Creating swapchain...\n");
+    VfpVkSwapChainSupportDetails swapChainSupport = {};
+    vfp_vk_query_swapchain_support(&swapChainSupport, pDevice->physicalDevice,
+                                   pDevice->surface);
+
+    VkSurfaceFormatKHR surfaceFormat = {};
+    vfp_vk_choose_swap_surface_format(
+        swapChainSupport.formats, swapChainSupport.capabilities.minImageCount,
+        &surfaceFormat);
+
+    VkPresentModeKHR presentMode = {};
+    vfp_vk_choose_swap_present_mode(swapChainSupport.presentModes,
+                                    swapChainSupport.capabilities.minImageCount,
+                                    &presentMode);
+    VkExtent2D extent = {};
+    vfp_vk_choose_swap_extent(&swapChainSupport.capabilities, &extent);
+
+    uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+    if (swapChainSupport.capabilities.maxImageCount > 0 &&
+        imageCount > swapChainSupport.capabilities.maxImageCount) {
+        imageCount = swapChainSupport.capabilities.maxImageCount;
+    }
+
+    printf("VulkanDevice // Creating swapchain with %d images...\n",
+           imageCount);
+
+    VkSwapchainCreateInfoKHR createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    createInfo.surface = pDevice->surface;
+    createInfo.minImageCount = imageCount;
+    createInfo.imageFormat = surfaceFormat.format;
+    createInfo.imageColorSpace = surfaceFormat.colorSpace;
+    createInfo.imageExtent = extent;
+    createInfo.imageArrayLayers = 1;
+    createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+    // Handle queue families
+    VfpVkQueueFamilyIndices queueFamilyIndices = {};
+    vfp_vk_find_queue_families(&queueFamilyIndices, pDevice,
+                               pDevice->physicalDevice);
+    uint32_t queueFamilyIndicesArray[] = {
+        (uint32_t)queueFamilyIndices.graphicsFamily,
+        (uint32_t)queueFamilyIndices.presentFamily};
+
+    if (queueFamilyIndices.graphicsFamily != queueFamilyIndices.presentFamily) {
+        printf("VulkanDevice // Swapchain using concurrent image sharing "
+               "mode...\n");
+        createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+        createInfo.queueFamilyIndexCount = 2;
+        createInfo.pQueueFamilyIndices = queueFamilyIndicesArray;
+    } else {
+        printf("VulkanDevice // Swapchain using exclusive image sharing "
+               "mode...\n");
+        createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        createInfo.queueFamilyIndexCount = 0;  // Optional
+        createInfo.pQueueFamilyIndices = NULL; // Optional
+    }
+
+    createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+    createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    createInfo.presentMode = presentMode;
+    createInfo.clipped = VK_TRUE;
+    createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+    if (vkCreateSwapchainKHR(pDevice->logicalDevice, &createInfo, NULL,
+                             &pDevice->swapchain) != VK_SUCCESS) {
+        fprintf(stderr, "VulkanDevice // Failed to create swapchain.\n");
+        return VFP_ERROR_GENERIC;
+    }
+
+    uint32_t swapchainImageCount = 0;
+    vkGetSwapchainImagesKHR(pDevice->logicalDevice, pDevice->swapchain,
+                            &swapchainImageCount, NULL);
+    pDevice->swapchainImageCount = swapchainImageCount;
+    pDevice->swapchainImages = malloc(sizeof(VkImage) * swapchainImageCount);
+    vkGetSwapchainImagesKHR(pDevice->logicalDevice, pDevice->swapchain,
+                            &swapchainImageCount, pDevice->swapchainImages);
+
+    pDevice->swapchainImageFormat = surfaceFormat.format;
+    pDevice->swapchainExtent = extent;
+    return VFP_OK;
+}
+
+VfpError vfp_vfk_create_image_views(VfpDeviceVulkan *pDevice) {
+
+    pDevice->swapchainImageViewCount = pDevice->swapchainImageCount;
+    pDevice->swapchainImageViews =
+        malloc(sizeof(VkImageView) * pDevice->swapchainImageViewCount);
+
+    // Loop over swapchain images and create image views
+    for (uint32_t i = 0; i < pDevice->swapchainImageCount; i++) {
+        VkImageViewCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        createInfo.image = pDevice->swapchainImages[i];
+        createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        createInfo.format = pDevice->swapchainImageFormat;
+        createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        createInfo.subresourceRange.baseMipLevel = 0;
+        createInfo.subresourceRange.levelCount = 1;
+        createInfo.subresourceRange.baseArrayLayer = 0;
+        createInfo.subresourceRange.layerCount = 1;
+
+        if (vkCreateImageView(pDevice->logicalDevice, &createInfo, NULL,
+                              &pDevice->swapchainImageViews[i]) != VK_SUCCESS) {
+            fprintf(stderr, "VulkanDevice // Failed to create image views for "
+                            "swapchain images.\n");
+            return VFP_ERROR_GENERIC;
+        }
+
+        return VFP_OK;
+    }
     return VFP_OK;
 }
