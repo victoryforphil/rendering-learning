8aaf80a9495e41ab033e669bd39f3f5c98c5e1c1
Alex Carter <victoryforphil@gmail.com>
2026-01-15 19:04:57 -0800

    Addec cglm

--- game-of-questioning-life/game/src/main.c
+++ game-of-questioning-life/game/src/main.c
@@ -1,12 +1,13 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
-
+#include <cglm/cglm.h>
 int main() {
     if (!glfwInit()) {
         printf("Failed to initialize GLFW\n");
         return -1;
     }
-
+    vec2 vector;
+    glm_vec2_zero(vector); 
     // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(640, 480, "Game of Questioning Life", NULL, NULL);
     if (!window) {
