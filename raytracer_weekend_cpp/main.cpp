81f8fe8fd656be68862dccea65bc065a2004eef3
VictoryForPhil <victoryforphil@gmail.com>
2026-05-02 19:07:00 -0700

    Ray Tracer // Basic PPM Class

--- raytracer_weekend_cpp/main.cpp
+++ raytracer_weekend_cpp/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <spdlog/spdlog.h>
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+#include <fstream> // Required for file operations
 int main() {
 
     spdlog::info("Welcome to spdlog!");
     spdlog::error("Some error message with arg: {}", 1);
-    
+
+    int image_width = 1280;
+    int image_height = 720;
+    std::ofstream MyFile("example.ppm");
+    MyFile << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int row = 0; row < image_height; row++) {
+        std::clog << "\rScanlines remaining: " << (image_height - row) << ' ' << std::flush;
+        for (int col = 0; col < image_width; col++) {
+            auto r =  static_cast<double>(col) / (image_width-1);
+            auto g = static_cast<double>(row) / (image_height-1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            MyFile << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    MyFile.close();
 
     // Test spdlog
-
+    std::clog << "\rDone.                 \n";
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
